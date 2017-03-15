9ceec44f8b96aa7db0819aa88b64c6a9cbbf98bb
CAMBRIDGE\amadrigal <amadrigal@cambridgeassociates.com>
2017-03-15 13:16:57 -0600

    Third commit

--- src/automationFramework/FirstTestCase.java
+++ src/automationFramework/FirstTestCase.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import java.io.IOException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstTestCase {;
+public class FirstTestCase {
 
 	public static void main(String[] args) throws IOException, Exception {
 		// TODO Auto-generated method stub
